73d191f8f42626669cbadab596c7c2a391dd42af
Marcus Prado <mgprado1@my.waketech.edu>
2026-04-23 08:01:31 -0400

    created minimum code to pass 2nd test of enter_garage to raise error

--- garage.py
+++ garage.py
@@ -1,5 +1,7 @@
 
 def enter_garage(garage, car_id, entry_hour):
+    if not isinstance(entry_hour, int):
+        raise TypeError("EntryHour must be a int")
     return garage
 
 def exit_garage(garage, car_id):
