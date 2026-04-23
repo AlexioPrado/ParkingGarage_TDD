184bad7150d800c11bf87fceaceba67cddb8ff47
Marcus Prado <mgprado1@my.waketech.edu>
2026-04-23 08:04:18 -0400

    created 3rd test to check value error of having same car ids in the garage

--- test_garage.py
+++ test_garage.py
@@ -7,3 +7,8 @@ def test_enter_garage_basic(garage):
 def test_enter_garage_invalid_hour(garage):
     with pytest.raises(TypeError):
         assert enter_garage(garage, 9, "6")
+
+def test_enter_garage_car_already_in_garage(garage):
+    with pytest.raises(ValueError):
+        enter_garage(garage, 1, 3)
+        assert enter_garage(garage, 1, 4)
